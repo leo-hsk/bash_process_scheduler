c8332930acebf758d61220945b8c7d3d48145cce
Leonard Husske <homerxd@t-online.de>
2020-04-18 19:48:20 +0200

    Adding the findwaitingtime function

--- FCFS.sh
+++ FCFS.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 
 declare -a wt=( $(for i in $(seq 1 $1); do echo 0; done) )
+declare -a bt=( 5 3 8 6 )
 
 
 function findWaitingTime() {
@@ -8,10 +9,25 @@ function findWaitingTime() {
 	declare -a service_time=( $(for i in $(seq 1 $1); do echo 0; done) )
 	
 
-	for ((i = 0; i < ${#service_time[@]}; i++));
-	do
-		echo "${service_time[$i]}";
-	done
+	# service_time[0] is always 0, because there is no previous process (executing). Same applies for wt[0], there 
+    # is no waiting time
+	service_time[0]=0
+    wt[0]=0
+
+    a=$3
+    echo $a
+
+    # calculating waiting time, iterate through processes. We start at 1 because we don't need to calculate the 
+    # service time for process[0], because there is no service time
+    n_minus_1=$(($1-1))
+    for i in $(seq 1 $n_minus_1)
+    do
+    	# Add burst time of previous processes  
+    	#service_time[$i]=$((service_time[$i] + $3[$i]))
+    	echo "${service_time[$i]}";
+    	#echo "$i"
+    done
+
 }
 
-findWaitingTime 4
+findWaitingTime 4 "$(echo ${wt[@]})" "$(echo ${bt[@]})"
