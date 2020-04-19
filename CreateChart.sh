7df26c506a50a4cd0e3f4f7b4eb11749ca23a7d6
Anton Rösler <anton.roesler98@gmx.de>
2020-04-19 16:44:54 +0200

    Time line indicator is now dynamic with the actual lenght

--- CreateChart.sh
+++ CreateChart.sh
@@ -3,7 +3,11 @@
 # Create some samples here that will later be replaced by the real output from the Algorithms
 process_names=(A B C D E F G)
 
-process_flow=( 0 0 0 0 0 0 0 0 0 0 0 0 0 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 5 5 5 5 5 5 5 3 3 3 3 3 3 3 3 3 3 3 2 2 2 2 2 2 2 2 2 2 1 1 1 1 1 2 2 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4 4 6 6 6 6 6 6 6 6)
+# Wichtig: Ich gehe hier davon aus das jedem Prozess der Reihenfolge nach eine nummer gegeben wird.
+# Also A -> 0, B -> 1, C -> 2 ... 
+# Und das in der Simulation nicht die namen in die Output Liste geschreiben werden, sondern eben die Nummer:
+# Die Output Liste sollte dann so aussehen:
+process_flow=( 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 2 2 2 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 5 5 5 5 5 5 5 3 3 )
 
 
 declare -a process_nums
@@ -13,7 +17,6 @@ len_names=${#process_names[@]}
 
 # For every name in the process names we want a number in process nums, satrting with 1:
 let x=$len_names-1
-echo $x
 for i in $(seq 0 $x)
 do 
     process_nums[$i]=$i
@@ -21,7 +24,7 @@ do
 done
 
 #echo ${process_nums[*]}
-echo ${output_data[*]}
+#echo ${output_data[*]}
 
 
 
@@ -41,17 +44,25 @@ do
 done
 
 
+
+# OUTPUT
+
+n_out=${#process_flow[@]}
+echo $n_out
+let n_remove=90-$n_out
+full_line="      1••••••••10••••••••20••••••••30••••••••40••••••••50••••••••60••••••••70••••••••80••••••••90"
+
+time_line=${full_line::${#full_line}-n_remove+1}
 echo  
 echo  
-echo '      1••••••••10••••••••20••••••••30••••••••40••••••••50••••••••60••••••••70••••••••80••••••••90'
-echo ${output_data[0]}
-echo ${output_data[1]}
-echo ${output_data[2]}
-echo ${output_data[3]}
-echo ${output_data[4]}
-echo ${output_data[5]}
-echo ${output_data[6]}
-echo '      1••••••••10••••••••20••••••••30••••••••40••••••••50••••••••60••••••••70••••••••80••••••••90'
+echo $time_line
+len_out_data=${#output_data[@]}
+let x=$len_out_data-1
+for i in $(seq 0 $x)
+do 
+    echo ${output_data[$i]}
+done
+#echo $time_line
 
 
 
