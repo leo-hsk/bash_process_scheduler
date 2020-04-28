4307d5b50d3f9e13ab38c10e346134e4fb1e6f62
Anton Rösler <anton.roesler98@gmx.de>
2020-04-28 16:55:23 +0200

    Calculate Waiting time

--- FCFS_with_waitingTime.sh
+++ FCFS_with_waitingTime.sh
@@ -16,6 +16,7 @@ declare -a bt=( 3 5 1 3 6)
 
 # Arrival Time
 declare -a at=( 3 5 7 2 6 )
+declare -a arrival_time=( 3 5 7 2 6 )
 
 # Waiting Time
 declare -a wt=( $(for i in $(seq 1 $n); do echo 0; done) )
@@ -23,6 +24,8 @@ declare -a wt=( $(for i in $(seq 1 $n); do echo 0; done) )
 # One if process is waiting and zero if not
 isWaiting=( $(for i in $(seq 1 $n); do echo 0; done) )
 
+# Turnaround Time
+declare -a tat=( $(for i in $(seq 1 $n); do echo 0; done) )
 
 #output of the "function"
 declare -a process_flow=()
@@ -81,53 +84,57 @@ function getAllWaitingJobs() {
 
 
 # Track the no. of iterations
-	clock=0
+clock=0
 
-	# Loop until the sum of the burst time array is zero
-	while [ $(IFS=+; echo "$((${bt[*]}))") -gt 0 ]
-	do
-		# Find index of the smallest arrival time
-		id=$(findSmallest)
-		let tmp=${at[$id]}-$clock
+# Loop until the sum of the burst time array is zero
+while [ $(IFS=+; echo "$((${bt[*]}))") -gt 0 ]
+do
+	# Find index of the smallest arrival time
+	id=$(findSmallest)
+	let tmp=${at[$id]}-$clock
 
-		if [[ $tmp -le 0 ]]
-		then
-			service_units=${bt[$id]}
+	if [[ $tmp -le 0 ]]
+	then
+		service_units=${bt[$id]}
 
-			# Iterate through service_units ( Number of bursts)
-			for burst in $(seq 1 $service_units)
+		# Iterate through service_units ( Number of bursts)
+		for burst in $(seq 1 $service_units)
+		do
+
+			getAllWaitingJobs
+			# Update waiting times each iteration for every process other than id
+			for p in $(seq 0 $((${#isWaiting[@]}-1))) 
 			do
-
-				getAllWaitingJobs
-				# Update waiting times each iteration for every process other than id
-				for p in $(seq 0 $((${#isWaiting[@]}-1))) 
-				do
-					if [[ $p -ne $id ]]
-					then
-						wt[$p]=$((wt[$p]+isWaiting[$p]))
-					fi
-				done
-				# Controlling
-				# echo "wt" ${wt[@]}
-				# echo $clock
-
-				# Add the id of the process to the process_flow array
-				process_flow[$(($clock))]=$id
-
-
-				# Add the number of service units to the clock
-				clock=$(($clock+1))
+				if [[ $p -ne $id ]]
+				then
+					wt[$p]=$((wt[$p]+isWaiting[$p]))
+				fi
 			done
+			# Controlling
+			# echo "wt" ${wt[@]}
+			# echo $clock
 
-			bt[$id]=0
-			at[$id]=$(($max_no+1)) # IMPORTANT To Do: the algorithm is only working if the arrival time is set to high number.
+			# Add the id of the process to the process_flow array
+			process_flow[$(($clock))]=$id
 
-		else
-			# If there is no process ready to process, add -1 to the process flow array
-			process_flow[$(($clock))]=-1
+
+			# Add the number of service units to the clock
 			clock=$(($clock+1))
+		done
 
-		fi
-	done
+		bt[$id]=0
+		at[$id]=$(($max_no+1)) # IMPORTANT To Do: the algorithm is only working if the arrival time is set to high number.
+		
+		# Calculate the turnaround time using the imortal arrival time array.
+		tat[$id]=$(($clock-${arrival_time[$id]}))
+	else
+		# If there is no process ready to process, add -1 to the process flow array
+		process_flow[$(($clock))]=-1
+		clock=$(($clock+1))
 
-	echo ${process_flow[@]}
+	fi
+done
+
+echo ${process_flow[@]}
+echo __w ${wt[@]}
+echo tat ${tat[@]}
