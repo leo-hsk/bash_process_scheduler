0ee1617ace6a147f0f836169aa91b9f963f02ab8
Anton Rösler <anton.roesler98@gmx.de>
2020-04-29 22:15:37 +0200

    Working on a problem in RR

--- RoundRobin.sh
+++ RoundRobin.sh
@@ -1,16 +1,38 @@
 #!/bin/bash
 
-n=4
+############ input
 
-# Names of the processes
-processes=( A B C D )
-process_nums=( 0 1 2 3 ) # ToDo Create with a function.
+# Max no. of gantt chart characters
+max_no=75
 
-# Burst Time
-bt=( 5 4 8 7 )
+# No. of (sample) processes
+n=5
+
+declare -a process_names=( A B C D E )
+
+# List of process IDs
+declare -a process_IDs=( 0 1 2 3 4 )
+
+# List of burst time
+declare -a bt=( 3 5 1 3 6 )
 
 # Arrival Time
-at=( 22 1 1 7 )
+declare -a at=( 0 2 1 2 6 ) # Copy (mutable)
+declare -a arrival_time=( 3 5 7 2 6 ) # Original (immutable)
+
+# Waiting Time
+declare -a wt=( $(for i in $(seq 1 $n); do echo 0; done) )
+
+# One if process is waiting and zero if not
+isWaiting=( $(for i in $(seq 1 $n); do echo 0; done) )
+
+# Turnaround Time
+declare -a tat=( $(for i in $(seq 1 $n); do echo 0; done) )
+
+#output of the "function"
+declare -a process_flow=()
+
+declare -a process_order=()
 
 
 quantum=3
@@ -19,31 +41,65 @@ clock=0  # This is the simulations clock = passed time since start.
 
 declare -a output
 
+function is_in_order() {
+    for p2 in ${process_order[@]}
+    do 
+        if [[ $p2 -eq $1 ]]
+        then
+            return 1
 
+        fi
+    done
+    return 0
+}
+
+function make_order() {
+    for p in ${process_IDs[@]}
+    do 
+        printf "start $p"
+        is_in_order $p
+        x=$?
+        printf "x is $x"
+        if [[ ${at[$p]} -le $clock ]]  && [[ $x -eq 0 ]]
+        then
+            process_order+=($p)
+            printf "p is $p"
+        fi
+
+
+
+    done
+}
+make_order
 while [ $(IFS=+; echo "$((${bt[*]}))") -gt 0 ]
 do
     processed=0
-    echo p1 $processed
-    for i in ${process_nums[@]}
+    echo xx ${process_order[@]}
+    for id in ${process_order[@]}
     do 
-        let time_to_start=${at[$i]}-$clock
-        if [ $time_to_start -le 0 ] # If ariival time minus the passed time is less or equal than 0, the process is ready to be serviced.
+        let time_to_start=${at[$id]}-$clock
+        if [ $time_to_start -le 0 ] # If arival time minus the passed time is less or equal than 0, the process is ready to be serviced.
             then
-                service_units=${bt[$i]}  # The Service time for round robin is the time slice / quantum but it might be the case that a processes as less than quantum units left.
+                service_units=${bt[$id]}  # The Service time for round robin is the time slice / quantum but it might be the case that a processes as less than quantum units left.
                 if [ $service_units -gt 0 ]
                 then
                     if [ $service_units -gt $quantum ]
                     then 
                         service_units=$quantum  # If the Process has more than 3 (or whatever value quantum has) time units of service left, it gets shortend to the value of quantum. 
                     fi
-                    let clock=$clock+$service_units  # Set the new clock time.
-                    let new_burst_time=${bt[$i]}-$service_units  # Set the new left burst time.
-                    bt[$i]=$new_burst_time
+                    for i in $( seq $clock $(($clock+$service_units-1)))
+                        do
+                            clock=$(($clock+1))
+                            make_order
+                        done
+                    
+                    let new_burst_time=${bt[$id]}-$service_units  # Set the new left burst time.
+                    bt[$id]=$new_burst_time
                     processed=1
                     echo p2 $processed
                     for _ in $(seq 1 $service_units)
                     do
-                        output+=$i' '
+                        output+=$id' '
                     done
                 fi            
                 
@@ -56,6 +112,7 @@ do
         echo notimg
         let clock=$clock+1  # And of course the time gets increased by one.
         output+=-1' '
+        make_order
     fi
 
 
--- test.sh
+++ test.sh
@@ -3,21 +3,11 @@
 #myvar="$( awk -v var="$a" -v var2="$b" 'BEGIN {print ((var/var2)+1)*100000}' )"
 #printf $myvar
 
-id=( 0 1 2 3 4 )
+clock=2
+service_units=3
 
-wt=( 3 5 7 2 6 )
-bt=( 3 5 1 3 6 )
+for i in $( seq $clock $(($clock+$service_units-1)))
+    do
+        echo hi
 
-repsonseR=( 0 0 0 0 0 )
-
-
-for p in ${id[@]}
-do
-	a=${wt[$p]}
-	b=${bt[$p]}
-	responseRatio="$( awk -v var="$a" -v var2="$b" 'BEGIN {print ((var/var2)+1)*100000}' )"
-	repsonseR[$p]=$responseRatio
-done
-
-echo ${repsonseR[@]}
-
+    done
