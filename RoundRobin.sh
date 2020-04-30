260885495b3fbc8f0d8dcdca339dd09743a5e909
Anton Rösler <anton.roesler98@gmx.de>
2020-04-30 16:21:13 +0200

    RoundRobin is (pretty sure) working correctly now

--- RoundRobin.sh
+++ RoundRobin.sh
@@ -14,10 +14,10 @@ declare -a process_names=( A B C D E )
 declare -a process_IDs=( 0 1 2 3 4 )
 
 # List of burst time
-declare -a bt=( 3 5 1 3 6 )
+declare -a bt=( 4 4 4 4 4 )
 
 # Arrival Time
-declare -a at=( 0 2 1 2 6 ) # Copy (mutable)
+declare -a at=( 0 4 3 1 0 ) # Copy (mutable)
 declare -a arrival_time=( 3 5 7 2 6 ) # Original (immutable)
 
 # Waiting Time
@@ -32,17 +32,17 @@ declare -a tat=( $(for i in $(seq 1 $n); do echo 0; done) )
 #output of the "function"
 declare -a process_flow=()
 
-declare -a process_order=()
+declare -a queue=()
 
 
-quantum=3
+quantum=1
 
 clock=0  # This is the simulations clock = passed time since start.
 
 declare -a output
 
 function is_in_order() {
-    for p2 in ${process_order[@]}
+    for p2 in ${queue[@]}
     do 
         if [[ $p2 -eq $1 ]]
         then
@@ -56,69 +56,95 @@ function is_in_order() {
 function make_order() {
     for p in ${process_IDs[@]}
     do 
-        printf "start $p"
-        is_in_order $p
-        x=$?
-        printf "x is $x"
-        if [[ ${at[$p]} -le $clock ]]  && [[ $x -eq 0 ]]
+        if [[ $p -ne $1 ]]
         then
-            process_order+=($p)
-            printf "p is $p"
+            is_in_order $p
+            x=$?
+            if [[ ${at[$p]} -le $clock ]]  && [[ $x -eq 0 ]]
+            then
+                queue+=($p)
+            fi
         fi
 
 
 
     done
 }
-make_order
+
+
+function get_next_in_queue(){
+    # This function returns the next character in the queue and at the same time removes it from the queue.
+    # If there is nothing in the queue it returns a negative 1.
+    printf "called"
+    if [[ ${#queue[@]} -gt 0 ]]
+    then
+        x=${queue[0]}
+        queue=("${queue[@]:1}")
+        printf "x is $x \n"
+        return $x
+    else
+        printf "else"
+        return 44
+
+    fi
+
+}
+make_order 44
+
 while [ $(IFS=+; echo "$((${bt[*]}))") -gt 0 ]
 do
-    processed=0
-    echo xx ${process_order[@]}
-    for id in ${process_order[@]}
-    do 
+
+    processed=0 # ToDo remove this indicator
+    get_next_in_queue
+    id=$?
+    echo id: $id
+    echo qu: ${queue[@]}
+    if [[ $id -ne 44 ]]
+    then 
         let time_to_start=${at[$id]}-$clock
         if [ $time_to_start -le 0 ] # If arival time minus the passed time is less or equal than 0, the process is ready to be serviced.
+        then
+            service_units=${bt[$id]}  # The Service time for round robin is the time slice / quantum but it might be the case that a processes as less than quantum units left.
+            echo $service_units
+            if [ $service_units -gt 0 ]
             then
-                service_units=${bt[$id]}  # The Service time for round robin is the time slice / quantum but it might be the case that a processes as less than quantum units left.
-                if [ $service_units -gt 0 ]
-                then
-                    if [ $service_units -gt $quantum ]
-                    then 
-                        service_units=$quantum  # If the Process has more than 3 (or whatever value quantum has) time units of service left, it gets shortend to the value of quantum. 
-                    fi
-                    for i in $( seq $clock $(($clock+$service_units-1)))
-                        do
-                            clock=$(($clock+1))
-                            make_order
-                        done
-                    
-                    let new_burst_time=${bt[$id]}-$service_units  # Set the new left burst time.
-                    bt[$id]=$new_burst_time
-                    processed=1
-                    echo p2 $processed
-                    for _ in $(seq 1 $service_units)
+                if [ $service_units -gt $quantum ]
+                then 
+                    service_units=$quantum  # If the Process has more than 3 (or whatever value quantum has) time units of service left, it gets shortend to the value of quantum. 
+                fi
+                for i in $( seq $clock $(($clock+$service_units-1)))
                     do
-                        output+=$id' '
+                        clock=$(($clock+1))
+                        make_order $id 
+                        echo clock $clock
+                        echo queue ${queue[@]}
                     done
-                fi            
+                    make_order 44
                 
-
+                let new_burst_time=${bt[$id]}-$service_units  # Set the new left burst time.
+                bt[$id]=$new_burst_time
+                processed=1
+                echo processed $processed
+                for _ in $(seq 1 $service_units)
+                do
+                    output+=$id' '
+                done
+                last=$id
+                echo last set to $last
+                echo end clock $clock
+                echo - - - - 
+            fi            
         fi
-    done
-    echo p3 $processed
-    if [ $processed -eq 0 ]  # If there isnt a Process to service st this time, a -1 gets added to the process flow output. 
-    then 
+    else
         echo notimg
         let clock=$clock+1  # And of course the time gets increased by one.
         output+=-1' '
-        make_order
+        make_order 44
     fi
 
 
-    echo bt ${bt[*]}
-    echo clock $clock
-    echo - - $i
+    echo endend clock $clock
+    echo - - - 
 done
 
 echo 
