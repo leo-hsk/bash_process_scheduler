3ae6280c4710933f4bd02606dba72cf6df3e8941
Patrick Frech <patrickfrech@Patricks-MBP.fritz.box>
2020-04-30 19:52:07 +0200

    small update wirk working case and simple loop

--- ScheduleMain.sh
+++ ScheduleMain.sh
@@ -2,18 +2,47 @@
 
 
 
-printf "\n Welcome to Project Scheduler SoSe 2020 - Engineering Business Information Systems.\n\n"
-printf "######\n"
+echo "\n Welcome to Project Scheduler SoSe 2020 - Engineering Business Information Systems.\n"
+echo "......................................................................................\n"
 
-printf "Please choose one of the following Options:\n\n"
-printf "1. Go to Simulations\n"
-printf "2. Get a Overview about the features\n"
-printf "3. Check Documentation and Help\n"
-printf "4. Exit this Program\n"
+echo "Please choose one of the following Options:\n\n"
+echo "1. Go to Simulations"
+echo "2. Get a Overview about the features"
+echo "3. Run FCFS Scheduling"
+echo "4. Show Gant Chart"
+echo "5. Exit the Program"
 
-echo "Please take one of the 4 Options and press Enter."
-read menu
-if [[ $menue -lt 4 ]]; then
+echo "Please take one of the 5 Options and press Enter."
+read choise
+
+case "$choise" in
+	1)
 	. ./start_screen.sh
-fi
+	;;
+		2)
+	 	cat ./manpage.txt
+	 	;;
+	 		3)
+			. ./FCFS.sh
+			;;
+				4)
+				. ./CreateChart.sh
+				;;
+					5)
+					Exit
+					;;
+					esac
+
+echo "Would you like to pick another Testrun ? (Y/N) \n"
+read yesno
+case "$yesno" in
+	y*|Y*)
+		. ./ScheduleMain.sh
+		;;
+			n*|N*)
+			echo "Goodbye"
+			Exit
+		esac
+
+
 
