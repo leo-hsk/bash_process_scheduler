6808a05f3e61a52af3988834aaa3658619db3ccd
Leonard Husske <homerxd@t-online.de>
2020-05-03 12:30:06 +0200

    Changed dir because it is used by more than 1 sript

---
+++ common/getAllWaitingJobs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#############################################################################################
+#                                                                                           #
+# This shell script fills a list with boolean values whether the process is waiting or not. #
+# Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
+#          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
+#          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
+#                                                                                           #
+# Copyright (c) 2020-2020 Anton Rösler, Leonard Hußke, Patrick Frech.  All Rights Reserved. #
+#                                                                                           #
+#############################################################################################
+
+function getAllWaitingJobs() {
+
+	# Reset the isWaiting array to all zeros
+	for i in $(seq 0 $((${#isWaiting[@]}-1)))
+	do
+		isWaiting[$i]=0
+	done
+
+	for p in ${process_IDs[@]}
+	do
+		if [[ $((at[$p]-$clock)) -le 0 ]]
+		then
+			isWaiting[$p]=1
+		fi
+	done
+	# Controlling
+	# echo "Finished" ${isWaiting[@]}
+
+}
