cf3de6a9e76d0bc261c399020bfe4ae847d3d39a
Anton Rösler <anton.roesler98@gmx.de>
2020-05-04 22:01:11 +0200

    Copies the arrival and burst time arrays

---
+++ common/copyArrays.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#############################################################################################
+#                                                                                           #
+# This shell script contains functions to copy the arrival and burdt time array.            #
+# Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
+#          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
+#          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
+#                                                                                           #
+# Copyright (c) 2020-2020 Anton Rösler, Leonard Hußke, Patrick Frech.  All Rights Reserved. #
+#                                                                                           #
+#############################################################################################
+
+
+function copyArrivalTime () {
+    for i in $(seq 0 $((${#arrival_time[@]}-1)))
+    do
+        echo hi
+        at[$i]=${arrival_time[$i]}
+    done
+}
+
+function copyBurstTime () {
+    for i in $(seq 0 $((${#burst_time[@]}-1)))
+    do
+        echo hi
+        bt[$i]=${burst_time[$i]}
+    done
+}
