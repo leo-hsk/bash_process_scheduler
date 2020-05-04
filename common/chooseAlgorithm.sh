1c3429fb09ec90c927d4665066ade3ef8c344953
Leonard Husske <homerxd@t-online.de>
2020-05-04 18:52:20 +0200

    function is working and returns string

---
+++ common/chooseAlgorithm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#############################################################################################
+#                                                                                           #
+# The user can select the algorithm he wants to run.                                        #
+# Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
+#          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
+#          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
+#                                                                                           #
+# Copyright (c) 2020-2020 Anton Rösler, Leonard Hußke, Patrick Frech.  All Rights Reserved. #
+#                                                                                           #
+#############################################################################################
+
+source ${processSchedulerWorkingDir}/common/selectOptions.sh
+
+echo "Select one algorithm using up/down keys and enter to confirm:"
+echo
+
+options=("FCFS" "HRRN" "RoundRobin")
+
+select_option "${options[@]}"
+choice=$?
+
+echo "${options[$choice]}"
