a27aa0608293b8fa767a0945190036490169d411
Anton Rösler <anton.roesler98@gmx.de>
2020-05-04 20:16:33 +0200

    Added header.

--- ROUNDROBIN/Run_RoundRobin.sh
+++ ROUNDROBIN/Run_RoundRobin.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
-############ input
+#############################################################################################
+#                                                                                           #
+# This is our implementation of the Round Robin scheduling algorithm.                       #
+# Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
+#          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
+#          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
+#                                                                                           #
+# Copyright (c) 2020-2020 Anton Rösler, Leonard Hußke, Patrick Frech.  All Rights Reserved. #
+#                                                                                           #
+#############################################################################################
+
 
 # Max no. of gantt chart characters
 max_no=75
 
 # No. of (sample) processes
-n=3
+n=5
 
-declare -a process_names=( A B C )
+declare -a process_names=( A B C D E )
 
 # List of process IDs
-declare -a process_IDs=( 0 1 2 )
+declare -a process_IDs=( 0 1 2 3 4 )
 
 # List of burst time
-declare -a bt=( 3 4 7 )
+declare -a bt=( 3 5 1 3 6)
 
 # Arrival Time
-declare -a at=( 0 0 3 ) # Copy (mutable)
-declare -a arrival_time=( 0 0 3 ) # Original (immutable)
+declare -a at=( 3 5 7 2 6 ) # Copy (mutable)
+declare -a arrival_time=( 3 5 7 2 6 ) # Original (immutable)
 
 # Waiting Time
 declare -a wt=( $(for i in $(seq 1 $n); do echo 0; done) )
@@ -31,83 +41,23 @@ declare -a tat=( $(for i in $(seq 1 $n); do echo 0; done) )
 
 #output of the "function"
 declare -a process_flow=()
+# One if process is waiting and zero if not
+isWaiting=( $(for i in $(seq 1 $n); do echo 0; done) )
+
+export processSchedulerWorkingDir=$(pwd)
 
 declare -a queue=()
 
 
-quantum=2
+quantum=2  # The RoundRobin time slice/qunatum.
 
 clock=0  # This is the simulations clock = passed time since start.
 
+source ${processSchedulerWorkingDir}/isInOrder.sh
+source ${processSchedulerWorkingDir}/makeOrder.sh
+source ${processSchedulerWorkingDir}/getNextInQueue.sh
+source ${processSchedulerWorkingDir}/getAllWaitingJobs.sh
 
-function isInOrder() {
-    # If the passsed process is alredy in the queue return 1. If not return 0.
-    for p2 in ${queue[@]}  # Loop all processes in queue.
-    do 
-        if [[ $p2 -eq $1 ]]  # If the one passed as an argument equals one of the ones inside queue.
-        then
-            return 1
-
-        fi
-    done
-    # If no process in queue equaled the passed one:
-    return 0
-}
-
-function makeOrder() {
-    # This function updates the queue list. 
-    for p in ${process_IDs[@]}  # Loop all processes there are.
-    do 
-        if [[ $p -ne $1 ]]  # One can pass a process_id as an argument and the one wont be put in queue.
-        then
-            isInOrder $p  # Check if the process is already in queue 1=YES, 0=NO.
-            x=$?  # Store the 1 or 0 in x.
-            if [[ ${at[$p]} -le $clock ]]  && [[ $x -eq 0 ]]  # Check if the process has already arrived. And is not already in queue.
-            then
-                queue+=($p)  # Only if the two conditions apply the process_id is added to tehe queue.
-            fi
-        fi
-
-
-
-    done
-}
-
-
-function getNextInQueue(){
-    # This function returns the next process_id in the queue and at the same time removes it from the queue.
-    # If there is nothing in the queue it returns a 44 (44 because -1 does not work - turns it into a 255 wegen zweierkompliment - and 44 is ok because the max number of processes is 25 anyway.).
-    if [[ ${#queue[@]} -gt 0 ]]
-    then
-        x=${queue[0]}
-        queue=("${queue[@]:1}")
-        return $x
-    else
-        return 44
-
-    fi
-
-}
-
-function getAllWaitingJobs() {
-
-    # Reset the isWaiting array to all zeros
-    for i in ${process_IDs[@]}
-    do
-        isWaiting[$i]=0
-    done
-
-    for p in ${process_IDs[@]}
-    do
-        if [[ $((at[$p]-$clock)) -le 0 ]] && [[ ${bt[$p]} -gt 0 ]]
-        then
-            isWaiting[$p]=1
-        fi
-    done
-    # Controlling
-    # echo "Finished" ${isWaiting[@]}
-
-}
 
 makeOrder 44
 
@@ -116,20 +66,20 @@ do
     getNextInQueue  # Get the next process in queue by calling the function.
     id=$?  # Store the output inside the variable id.
     if [[ $id -ne 44 ]]  # The function returns a 44 in the case the queue is empty, meaning no process is waiting -> skip to else to increase clock by one.
-    then 
+    then
         service_units=${bt[$id]}  # The Service time for round robin is the time slice / quantum but it might be the case that a processes as less than quantum units left.
-        if [ $service_units -gt 0 ]  # If service units is greater then 0 -> continue. If it is 0 the process is already fully completed. 
+        if [ $service_units -gt 0 ]  # If service units is greater then 0 -> continue. If it is 0 the process is already fully completed.
         then
             if [ $service_units -gt $quantum ]  # In the likely case the service units are larger than the time quantum: reduce to quantum.
-            then 
-                service_units=$quantum  # If the Process has more than 3 (or whatever value quantum has) time units of service left, it gets shortend to the value of quantum. 
-            fi 
+            then
+                service_units=$quantum  # If the Process has more than 3 (or whatever value quantum has) time units of service left, it gets shortend to the value of quantum.
+            fi
             for i in $( seq 1 $service_units )  # Now do the following steps for *service_units* times.
                 do
 
                     getAllWaitingJobs
                     # Update waiting times each iteration for every process other than id
-                    for p in $(seq 0 $((${#isWaiting[@]}-1))) 
+                    for p in $(seq 0 $((${#isWaiting[@]}-1)))
                     do
                         if [[ $p -ne $id ]]
                         then
@@ -155,9 +105,9 @@ do
             # Update the burst time.
             bt[$id]=$((${bt[$id]}-$service_units))
             # Calculate the turnaround time using the immutable arrival time array.
-            tat[$id]=$(($clock-${arrival_time[$id]})) 
+            tat[$id]=$(($clock-${arrival_time[$id]}))
 
-        fi           
+        fi
     else
         # If there is no process ready to process, add -1 to the process flow array.
         process_flow[$clock]=-1
@@ -168,7 +118,7 @@ do
 
 done
 
-echo 
+echo
 echo ${process_flow[@]}
 echo __w ${wt[@]}
 echo tat ${tat[@]}
