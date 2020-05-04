b487de05748f6903d4aa006823aef7b6fb134574
Leonard Husske <homerxd@t-online.de>
2020-05-04 21:29:57 +0200

    testing the functions

--- common/processManagement.sh
+++ common/processManagement.sh
@@ -11,9 +11,11 @@
 #                                                                                           #
 #############################################################################################
 
+export processSchedulerWorkingDir=$(pwd)
 processesFilePath=${releaseCreatorWorkingDir}/.processes.log
 
 function setProcesses() {
+	touch .processes.log
 	######## Needs to be implemented
 	# Create hidden file
 	# save processes into the file (name, waiting time, burst time, maybe no of processes?)
@@ -21,18 +23,22 @@ function setProcesses() {
 }
 
 function getProcesses() {
+	echo
 	######## Needs to be implemented
 	# read data from file
 	# add data to arrays
 }
 
 function remProcesses() {
-  rm -f ${processesFilePath}
+	rm -f ${processesFilePath}
 }
 
 function checkIfProcessesExist() {
-	######## Needs to be implemented
-	# Check if file exists
-	# If not: print message that there are no saved processses and exit script
+	if [ -f "$processesFilepath" ]
+	then
+    	echo 1
+	fi
 
 }
+
+setProcesses 1
