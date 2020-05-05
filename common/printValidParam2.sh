039a09c413d5697bc13f06d61bf90192612bfb71
Patrick Frech <patrickfrech@Patricks-MBP.fritz.box>
2020-05-05 21:21:26 +0200

    Helpfile parameters

---
+++ common/printValidParam2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#############################################################################################
+#                                                                                           #
+# This shell script prints all valid param2 for this program.                               #
+# Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
+#          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
+#          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
+#                                                                                           #
+# Copyright (c) 2020-2020 Anton Rösler, Leonard Hußke, Patrick Frech.  All Rights Reserved. #
+#                                                                                           #
+#############################################################################################
+
+export validParam2=("--FCFS" \ 
+			 "--HRRN" \
+			 "--RoundRobin"	)
+
+for i in ${validParam2[@]}; do
+	echo $i
+	
+done
