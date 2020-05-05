e65aeab3de9a12b712f407eeed82b9dce14204b5
Leonard Husske <homerxd@t-online.de>
2020-05-05 22:34:21 +0200

    Adding printhelp and helper function back in

--- common/printHelp.sh
+++ common/printHelp.sh
@@ -2,7 +2,7 @@
 
 #############################################################################################
 #                                                                                           #
-# This shell script prints all required information to run scripts.                        #
+# This shell script prints the usage of the Process Scheduler Simulation.                   #
 # Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
 #          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
 #          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
@@ -12,8 +12,8 @@
 #############################################################################################
 
 echo ""
-echo ""
-echo "Usage: Start_Process_Scheduler_Simulation.sh [opt:param1] [opt:param2]"
+echo "Usage:"
+echo "Start_Process_Scheduler_Simulation.sh [opt:param1] [opt:param2]"
 echo ""
 echo "param1 is optional and can be one of the described commands."
 echo "param2 is only used with param1=help has to be a valid string."
@@ -24,6 +24,6 @@ echo "--help [param2]  - Starts the help with the given param2 for specific algo
 echo ""
 echo "Valid param2's are: "
 echo ""
-${processSchedulerWorkingDir}/common/printValidParam2.sh
+source ${processSchedulerWorkingDir}/common/printValidParam2.sh
 echo ""
 echo ""
--- common/printValidParam2.sh
+++ common/printValidParam2.sh
@@ -2,7 +2,7 @@
 
 #############################################################################################
 #                                                                                           #
-# This shell script prints all valid param2 for this program.                               #
+# This shell script prints all valid param2's for this program.                             #
 # Authors: Anton Rösler (anton.roesler@stud.fra-uas.de)                                     #
 #          Leonard Hußke (leonard.husske@stud.fra-uas.de)                                   #
 #          Patrick Frech (patrick.frech@stud.fra-uas.de)                                    #
@@ -10,10 +10,6 @@
 # Copyright (c) 2020-2020 Anton Rösler, Leonard Hußke, Patrick Frech.  All Rights Reserved. #
 #                                                                                           #
 #############################################################################################
-
-export validParam2=("--FCFS" \ 
-			 "--HRRN" \
-			 "--RoundRobin"	)
 
 for i in ${validParam2[@]}; do
 	echo $i
