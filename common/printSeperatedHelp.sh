5b17b5809c86fb6272590da8d152ace11f7634f3
Leonard Husske <homerxd@t-online.de>
2020-05-07 14:52:09 +0200

    Added new help texts

--- common/printSeperatedHelp.sh
+++ common/printSeperatedHelp.sh
@@ -14,90 +14,84 @@
 case "$2" in
       
       --resume)
-          # give first parameter to used script
-          echo "run the progamm with the last given paramters"
+          echo ""
+          echo "--resume"
+          echo ""
+          echo "The resume option loads the processes of the last session."
+          echo ""
+          echo ""
            exit 0
            ;;
 
       --HRRN)
-          # give second parameter to used script
+
           echo ""
-          echo "###############################################################"
-          echo "#          HRRN algorithm overview and  function              #"
-          echo "###############################################################"
+          echo "Highest Response Ratio Next (HRRN)"
           echo ""
-          echo "Given n processes with their Arrival times and Burst times," 
-          echo "the task is to find average waiting time and average turn around"
-          echo "time using HRRN scheduling algorithm. The name itself states that"
-          echo "need to find the response ratio of all available processes and"
-          echo "select the one with the highest Response Ratio. A process once "
-          echo "selected will run till completion."
+          echo "HRRN is a non-preemptive scheduling algorithm that always "
+          echo "assigns the process with the highest response ratio to the "
+          echo "processor. The processor executes the process until the "
+          echo "process does not have any Burst Time left. The quotient is"
+          echo "calculated as follows:"
+          echo ""
+          echo "      ResponseRatio = (Waiting Time / Burst Time) + 1"
+          echo ""
+          echo ""
            exit 0
            ;;
 
       --FCFS) 
-      # explain 
           echo ""
-          echo "###############################################################"
-          echo "#          FCFS algorithm overview and  function              #"
-          echo "###############################################################"
+          echo "First Come First Served (FCFS)"
           echo ""
-      	  echo "Given n processes with their burst times, the task is to find"
-      	  echo "average waiting time and average turn around time using FCFS "
-      	  echo "scheduling algorithm. First in, first out (FIFO), also known as "
-      	  echo "first come, first served (FCFS), is the simplest scheduling "
-      	  echo "algorithm. FIFO simply queues processes in the order that they  "
-      	  echo "arrive in the ready queue. In this, the process that comes first "
-      	  echo "will be executed first and next process starts only after the "
-          echo "previous gets fully executed. Here we are considering that  "
-          echo "arrival time for all processes is 0."
+      	  echo "FCFS is a non-preemptive scheduling algorithm. FCFS simply "
+          echo "queues processes in the order that they arrive in the ready "
+      	  echo "queue. The process that comes first will be executed first"
+      	  echo "and the next process only starts after the previous process  "
+          echo "was finished."
+          echo ""
+          echo ""
           exit 0
           ;;
 
       --RoundRobin) 
-      # explain
           echo ""
-          echo "###############################################################"
-          echo "#         RoundRobin algorithm overview and  function         #"
-          echo "###############################################################"
+          echo "Round Robin"
           echo ""
-      	  echo "is one of the algorithms employed by process and network "
-      	  echo "schedulers in computing. As the term is generally used, time  "
-      	  echo "slices (also known as time quanta) are assigned to each process "
-      	  echo "in equal portions and in circular order, handling all processes  "
-      	  echo "without priority (also known as cyclic executive). Round-robin  "
-      	  echo "scheduling is simple, easy to implement, and starvation-free.  "
-      	  echo "Round-robin scheduling can be applied to other scheduling  "
-      	  echo "problems, such as data packet scheduling in computer networks. "
-      	  echo "It is an operating system concept."
+      	  echo "Round Robin is a preemptive scheduling algorithm. That means"
+      	  echo "that the processor allocates its ressources for a limited "
+      	  echo "amount of time to one process before it switches to another"
+      	  echo "process. If the already executed process still has remaining "
+      	  echo "Burst Time, then the process is placed back in the ready "
+      	  echo "queue. That process stays in the ready queue till it gets  "
+      	  echo "next chance to execute. You can adjust the amount of time"
+      	  echo "the Process Scheduling Simulator executes a process by  "
+      	  echo "increasing or decreasing the time slice (time quantum)."
+          echo ""
+          echo ""
           exit 0
           ;;
 
       --chart) 
-      # explain
           echo ""
-          echo "###############################################################"
-          echo "#         RoundRobin algorithm overview and  function         #"
-          echo "###############################################################"
+          echo "Example Gantt Chart of FCFS
+
+     1••••••••10••••••••20
+A    ░░░▒▒███░░░░░░░░░░░░
+B    ░░░░░▒▒▒█████░░░░░░░
+C    ░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒█
+D    ░░███░░░░░░░░░░░░░░░
+E    ░░░░░░▒▒▒▒▒▒▒██████░
+     1••••••••10••••••••20
+"
           echo ""
-          echo "is one of the algorithms employed by process and network "
-          echo "schedulers in computing. As the term is generally used, time  "
-          echo "slices (also known as time quanta) are assigned to each process "
-          echo "in equal portions and in circular order, handling all processes  "
-          echo "without priority (also known as cyclic executive). Round-robin  "
-          echo "scheduling is simple, easy to implement, and starvation-free.  "
-          echo "Round-robin scheduling can be applied to other scheduling  "
-          echo "problems, such as data packet scheduling in computer networks. "
-          echo "It is an operating system concept."
+          echo "  ░: Process not in queue or already executed."
+          echo "  ▒: Process in queue ('Waiting') but not executed."
+          echo "  █: Process executed."
+          echo ""
           exit 0
           ;;
 
-
-      --*)
-          echo "Error: Unknown option: $1" >&2
-          ## or call function display_help
-          exit 1 
-          ;;
       *)  # No more options
           break
           ;;
