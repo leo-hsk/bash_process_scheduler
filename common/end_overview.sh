8935d25706f9381a8f2c00b7cc5b7c840f54f01c
Anton Rösler <anton.roesler98@gmx.de>
2020-05-07 21:42:26 +0200

    better layout

--- common/end_overview.sh
+++ common/end_overview.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-awk 'BEGIN {printf("%-10s %-10s %-10s %-10s %-10s  \n" ,"Process", "AT", "BT", "TAT", "WT")}'
+awk 'BEGIN {printf("%-12s %-12s %-12s %-12s %-12s  \n" ,"Process", "AT", "BT", "TAT", "WT")}'
 for i in ${process_IDs[@]}
 do
     echo "------------------------------------------------------------"
-    awk -v p_names="${process_names[$i]::8}" -v at="${arrival_time[$i]}" -v bt="${burst_time[$i]}" -v tt="${tat[$i]}" -v wtt="${wt[$i]}" 'BEGIN {printf("%-10s %-10s %-10s %-10s %-10s  \n" ,p_names, at, bt, tt, wtt)}'
+    awk -v p_names="${process_names[$i]::8}" -v at="${arrival_time[$i]}" -v bt="${burst_time[$i]}" -v tt="${tat[$i]}" -v wtt="${wt[$i]}" 'BEGIN {printf("%-12s %-12s %-12s %-12s %-12s  \n" ,p_names, at, bt, tt, wtt)}'
 done
