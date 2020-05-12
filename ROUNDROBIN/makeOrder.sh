6dc0a179fcfa53a38cef168824d9d75ed9e66cd6
Leonard Husske <homerxd@t-online.de>
2020-05-12 09:47:57 +0200

    Changed spelling mistake in comment

--- ROUNDROBIN/makeOrder.sh
+++ ROUNDROBIN/makeOrder.sh
@@ -15,13 +15,13 @@
 function makeOrder() { 
     for p in ${process_IDs[@]}  # Loop all processes there are.
     do 
-        if [[ $p -ne $1 ]]  # One can pass a process_id as an argument and the one wont be put in queue.
+        if [[ $p -ne $1 ]]  # One can pass a process_id as an argument and the one won't be put in queue.
         then
             isInOrder $p  # Check if the process is already in queue 1=YES, 0=NO.
             x=$?  # Store the 1 or 0 in x.
             if [[ ${at[$p]} -le $clock ]]  && [[ $x -eq 0 ]]  # Check if the process has already arrived. And is not already in queue.
             then
-                queue+=($p)  # Only if the two conditions apply the process_id is added to tehe queue.
+                queue+=($p)  # Only if the two conditions apply the process_id is added to the queue.
             fi
         fi
     done
